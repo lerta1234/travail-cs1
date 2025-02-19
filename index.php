6c72a99a7f81e932e17e272878f7c8d64c221ade
lerta1234 <marlaewitha@gmail.com>
2025-02-19 18:35:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale)1.0">
+        <title> Home page</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+
+
+<p>BIENVENU MUNDELE</p>
+
+<marquee>TRAVAIL EFFECTUE</marquee>
 <?php
  require_once('./config/database.php');
  $db= new ConnexionDB();
@@ -8,3 +22,9 @@
 	 echo "Erreur de connexion";
  }
 ?>
+
+
+</body>
+
+
+</html>
